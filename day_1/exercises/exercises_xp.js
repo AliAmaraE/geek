0840c63477cfaab5b2884f35337ca709008c6995
AliAmaraE <ali.amara.a01@gmail.com>
2026-01-27 11:04:27 +0100

    Update exercises_xp.js and exercises_xp_gold.js

--- day_1/exercises/exercises_xp.js
+++ day_1/exercises/exercises_xp.js
@@ -1,4 +1,32 @@
 // ===== Exercise 1
+const people = ["Greg", "Mary", "Devon", "James"];
+people.shift(); 
+console.log(people);
+people[people.indexOf("James")] = "Jason";
+console.log(people);
+people.push("Ali"); 
+console.log(people); 
+console.log(people.indexOf("Mary")); 
+console.log(peopleCopy); 
+const partialCopy = people.slice(1, people.length - 1);
+console.log(partialCopy); 
+console.log(people.indexOf("Foo")); 
+const last = people[people.length - 1];
+console.log(last); 
+// =========================Part II - Loops=================
+
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i]);
+}
+for (const person of people) {
+  console.log(person);
+}
+for (const person of people) {
+  console.log(person);
+  if (person === "Devon") {
+    break;
+  }
+}
 
 // ===== Exercise 2
 
--- day_1/exercises/exercises_xp_gold.js
+++ day_1/exercises/exercises_xp_gold.js
@@ -1,33 +1,4 @@
 // ===== Exercise 1
-const people = ["Greg", "Mary", "Devon", "James"];
-people.shift(); 
-console.log(people);
-people[people.indexOf("James")] = "Jason";
-console.log(people);
-people.push("Ali"); // replace "Ali" with your name
-console.log(people); // ["Mary", "Devon", "Jason", "Ali"]
-console.log(people.indexOf("Mary")); // 0
-const peopleCopy = people.slice(); // copies the entire array
-console.log(peopleCopy); // ["Mary", "Devon", "Jason", "Ali"]
-const partialCopy = people.slice(1, people.length - 1);
-console.log(partialCopy); // ["Devon", "Jason"]
-console.log(people.indexOf("Foo")); // -1
-const last = people[people.length - 1];
-console.log(last); // "Ali"
-// =========================Part II - Loops=================
-
-for (let i = 0; i < people.length; i++) {
-  console.log(people[i]);
-}
-for (const person of people) {
-  console.log(person);
-}
-for (const person of people) {
-  console.log(person);
-  if (person === "Devon") {
-    break;
-  }
-}
 
 // ===== Exercise 2
 
