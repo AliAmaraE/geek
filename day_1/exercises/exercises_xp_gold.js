959448f029fbd02c194c5ca47e374d95d88372cc
AliAmaraE <ali.amara.a01@gmail.com>
2026-01-28 16:35:16 +0100

    Update exercises XP solutions

--- day_1/exercises/exercises_xp_gold.js
+++ day_1/exercises/exercises_xp_gold.js
@@ -1,8 +1,54 @@
 // ===== Exercise 1
+let numbers = [123, 8409, 100053, 333333333, 7];
 
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 3 === 0) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
 // ===== Exercise 2
+let guestList = {
+  randy: "Germany",
+  karla: "France",
+  wendy: "Japan",
+  norman: "England",
+  sam: "Argentina"
+};
+
+let name = prompt("Enter your name:");
+
+
+if (name in guestList) {
+  console.log(`Hi! I'm ${name}, and I'm from ${guestList[name]}.`);
+} else {
+  console.log("Hi! I'm a guest.");
+}
+
 
 // ===== Exercise 3
+
+let age = [20, 5, 12, 43, 98, 55];
+
+let sum = 0;
+
+for (let i = 0; i < age.length; i++) {
+  sum = sum + age[i];
+}
+
+console.log("Sum of ages:", sum);
+
+let maxAge = age[0];
+
+for (let i = 1; i < age.length; i++) {
+  if (age[i] > maxAge) {
+    maxAge = age[i];
+  }
+}
+
+console.log("Highest age:", maxAge);
+
 
 // ===== Exercise 4
 
